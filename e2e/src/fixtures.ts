9d88cbd945b8398c96ea93aa8891b7765abb1f58
Matt Raible <matt.raible@crowdstrike.com>
2026-04-23 08:46:27 -0600

    Migrate e2e tests to @crowdstrike/foundry-playwright

--- e2e/src/fixtures.ts
+++ e2e/src/fixtures.ts
@@ -1,10 +1,7 @@
 import { test as baseTest } from '@playwright/test';
-import { FoundryHomePage } from './pages/FoundryHomePage';
-import { AppManagerPage } from './pages/AppManagerPage';
-import { AppCatalogPage } from './pages/AppCatalogPage';
-import { WorkflowsPage } from './pages/WorkflowsPage';
-import { config } from './config/TestConfig';
-import { logger } from './utils/Logger';
+import {
+  FoundryHomePage, AppManagerPage, AppCatalogPage, WorkflowsPage, config,
+} from '@crowdstrike/foundry-playwright';
 
 type FoundryFixtures = {
   foundryHomePage: FoundryHomePage;
@@ -15,37 +12,11 @@ type FoundryFixtures = {
 };
 
 export const test = baseTest.extend<FoundryFixtures>({
-  page: async ({ page }, use) => {
-    const timeouts = config.getPlaywrightTimeouts();
-    page.setDefaultTimeout(timeouts.timeout);
-
-    if (!process.env.CONFIG_LOGGED) {
-      config.logSummary();
-      process.env.CONFIG_LOGGED = 'true';
-    }
-
-    await use(page);
-  },
-
-  foundryHomePage: async ({ page }, use) => {
-    await use(new FoundryHomePage(page));
-  },
-
-  appManagerPage: async ({ page }, use) => {
-    await use(new AppManagerPage(page));
-  },
-
-  appCatalogPage: async ({ page }, use) => {
-    await use(new AppCatalogPage(page));
-  },
-
-  workflowsPage: async ({ page }, use) => {
-    await use(new WorkflowsPage(page));
-  },
-
-  appName: async ({}, use) => {
-    await use(config.appName);
-  },
+  foundryHomePage: async ({ page }, use) => { await use(new FoundryHomePage(page)); },
+  appManagerPage: async ({ page }, use) => { await use(new AppManagerPage(page)); },
+  appCatalogPage: async ({ page }, use) => { await use(new AppCatalogPage(page)); },
+  workflowsPage: async ({ page }, use) => { await use(new WorkflowsPage(page)); },
+  appName: async ({}, use) => { await use(config.appName); },
 });
 
 export { expect } from '@playwright/test';
